1735fd51d07dc7f4cd46d29b9df18f3a03bada3d
sachidanand33 <sachidanand.shahane86@gmail.com>
2026-08-10 21:05:58 +0530

    Revert "Merge pull request #4 from sachidanand33/sachidanand-terraform"
    This reverts commit e49fd4e594b28394dc3ba1ab4008cca3ebb2e972, reversing
    changes made to fc69f5df21879d6616b5757d19dc440bbb9828e8.

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_s3_bucket" "fist_bucket" {
   bucket = "aws-terraform-596055752724"
 
   tags = {
-    Name        = "My bucket-01"
+    Name        = "My bucket"
     Environment = "Dev"
   }
 }
