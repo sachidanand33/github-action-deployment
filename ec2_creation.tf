e7f605dc3c421c348aca8eec15f14db8db80b40b
sachidanand33 <sachidanand.shahane86@gmail.com>
2026-08-19 19:26:35 +0530

    Initial Terraform create infra in us-east-1

--- ec2_creation.tf
+++ ec2_creation.tf
@@ -52,7 +52,7 @@ resource "aws_security_group" "web_sg" {
 resource "aws_instance" "web" {
   count = 2
 
-  ami           = "ami-00d2dbb426772b03a"
+  ami           = "ami-0332d564d76dbd8d6"
   instance_type = "t2.micro"
 
   # Launch all EC2 instances inside our VPC subnet
